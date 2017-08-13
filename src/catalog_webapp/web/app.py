87375984d1cc87d9218d2e3a2c8bd5117760d464
Ratnadeep Abrol <ratna.abrol@googlemail.com>
2017-08-13 11:53:36 +0100

    Added no-op register route.

--- src/catalog_webapp/web/app.py
+++ src/catalog_webapp/web/app.py
@@ -1,5 +1,5 @@
 """The web application entry point for the catalog web application"""
-from flask import Flask
+from flask import Flask, render_template
 
 
 APP = Flask(__name__)
@@ -9,3 +9,8 @@ APP = Flask(__name__)
 def index():
     """Main page."""
     return "This is the temporary index page for the catalog application."
+
+@APP.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    return render_template("register.html")
